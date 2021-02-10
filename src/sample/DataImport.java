00ec38312a492bc62618cbedd52ea82b89b10ecf
brillew <brillew@rose-hulman.edu>
2021-02-11 07:11:20 +0800

    Began the process of data import

--- src/sample/DataImport.java
+++ src/sample/DataImport.java
@@ -15,13 +15,23 @@ public class DataImport {
 
     public void importData (String fileName) throws IOException{
         Workbook wb = WorkbookFactory.create(new File(fileName));
-        Sheet dataIn = wb.getSheetAt(0);
+        Sheet trainingLog = wb.getSheetAt(0);
+        Sheet performanceList = wb.getSheetAt(1);
         DataFormatter df = new DataFormatter();
-        for (int i = 1; i < dataIn.getLastRowNum() + 1; i++) {
-            for (int k = 1; k < dataIn.getRow(i).getLastCellNum(); k++) {
-                String cellValue = df.formatCellValue(dataIn.getRow(i).getCell(k));
+        for(int i = 1; i < performanceList.getLastRowNum() + 1; i++){
+            for(int k = 0; k < performanceList.getRow(i).getLastCellNum(); k++){
+                String cellValue = df.formatCellValue(performanceList.getRow(i).getCell(k));
+                //System.out.println(cellValue);
+            }
+        }
+        for (int i = 1; i < trainingLog.getLastRowNum() + 1; i++){
+            for (int k = 0; k < trainingLog.getRow(i).getLastCellNum(); k++) {
+                String cellValue = df.formatCellValue(trainingLog.getRow(i).getCell(k));
+                //System.out.println(cellValue);
             }
         }
         wb.close();
     }
+
+
 }
